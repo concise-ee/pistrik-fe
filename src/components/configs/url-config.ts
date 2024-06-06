110dc9b6d6eed6e1dff3a34e6d54a0a5e134ea2e
liinaanette <liinaanette@gmail.com>
2024-06-06 16:19:28 +0300

    correctly get BE URL

--- src/components/configs/url-config.ts
+++ src/components/configs/url-config.ts
@@ -1,5 +1,4 @@
-const { REACT_APP_BE_URL } = process.env;
-
-export const BE_URL = REACT_APP_BE_URL || 'http://127.0.0.1:59564/';
+// @ts-ignore
+export const BE_URL = window.BE_URL || 'http://127.0.0.1:59564/';
 export const INADDRESS_BASE_URL = 'https://inaadress.maaamet.ee/inaadress/';
 export const ARIREGISTER_BASE_URL = 'https://ariregister.rik.ee/est/api/autocomplete?';
